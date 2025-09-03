4e1b0ef4887884b2a87b9463f685a524fb72f561
Eduardo Prestes <eduardo.prestes@facens.br>
2025-09-03 10:36:24 -0300

    chore(admin): register diagnostic report in admin
    Register DiagnosticReport with list filters/search.

--- diagnostics/admin.py
+++ diagnostics/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
-from .models import DiagnosticReport
+from .models import DiagnosticReport, ImageAttachment, VideoAttachment
+
+class ImageInline(admin.TabularInline):
+    model = ImageAttachment
+    extra = 0
+
+class VideoInline(admin.TabularInline):
+    model = VideoAttachment
+    extra = 0
 
 @admin.register(DiagnosticReport)
 class DiagnosticReportAdmin(admin.ModelAdmin):
@@ -7,3 +15,12 @@ class DiagnosticReportAdmin(admin.ModelAdmin):
     list_filter = ("category", "created_at")
     search_fields = ("title", "su_identifier", "user_name", "user_email", "message")
     ordering = ("-created_at",)
+    inlines = [ImageInline, VideoInline]
+
+@admin.register(ImageAttachment)
+class ImageAttachmentAdmin(admin.ModelAdmin):
+    list_display = ("id", "report", "created_at")
+
+@admin.register(VideoAttachment)
+class VideoAttachmentAdmin(admin.ModelAdmin):
+    list_display = ("id", "report", "created_at")
